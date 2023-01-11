5f909b4b5ee346859674d337df3140066176be93
pieterdepetter <101345533+pieterdepetter@users.noreply.github.com>
2023-01-11 14:48:16 +0100

    Update lynx_client.py

--- src/pyveb/lynx_client.py
+++ src/pyveb/lynx_client.py
@@ -28,28 +28,31 @@ class lynxClient():
         try: 
             server_raw = os.environ['LYNX_SERVER']
             port = os.environ['LYNX_PORT']
-            database = os.environ['LYNX_DATABASE']
-            username = os.environ['LYNX_USERNAME']
-            password = os.environ['LYNX_PASSWORD']
-            server = f'{server_raw},{port}'
+            self.database = os.environ['LYNX_DATABASE']
+            self.username = os.environ['LYNX_USERNAME']
+            self.password = os.environ['LYNX_PASSWORD']
+            self.server = f'{server_raw},{port}'
             logging.info("Fetched lynx credentials from environment variables")
         except Exception as e:
             logging.error("Issue fetching lynx credentials from environment variables. Exiting...")
             logging.error(e)
             sys.exit(1)
+        self._connection_instance = None
+        self._connect()
+        logging.info('connected to lynx via pyodbc')
 
-        # connect to lynx SQL server 
-        try: 
-            connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
-            self.conn = self._connect(connection_string)
-            logging.info("Succesfully set up connection with Lynx SQL Server")
-        except Exception as e:
-            logging.error("Unable to establish connection with Lynx SQL Server. Exiting")
-            logging.error(e)
-            sys.exit(1)
-        return
+        # # connect to lynx SQL server 
+        # try: 
+        #     self.connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
+        #     self.conn = self._connect(self.connection_string)
+        #     logging.info("Succesfully set up connection with Lynx SQL Server")
+        # except Exception as e:
+        #     logging.error("Unable to establish connection with Lynx SQL Server. Exiting")
+        #     logging.error(e)
+        #     sys.exit(1)
+        # return
 
-    def _connect(self,connection_string:str):
+    def _connect(self):
         f"""
             Connect to Lynx via pyodbc. Automatically retries {self.CONN_RETRIES} times with exponential backoff of {self.BACKOFF_IN_SECONDS}. 
             Returns Connection or runtime error
@@ -57,23 +60,25 @@ class lynxClient():
         # https://github.com/mkleehammer/pyodbc/wiki/Unicode
         # conn.setdecoding(pyodbc.SQL_CHAR, encoding='latin1', to=str)
         # conn.setencoding(str, encoding='latin1')
-        nbr_of_retries = 0
-        while True:
-            try:
-                return pyodbc.connect(connection_string)
-            except pyodbc.Error as ex:
-                if nbr_of_retries == int(self.CONN_RETRIES)-1:
-                    raise RuntimeError(f'Not able to establish connection with Lynx server after {self.CONN_RETRIES}') from ex
-                sleep_duration = (int(self.BACKOFF_IN_SECONDS) * 2 ** nbr_of_retries + random.uniform(0, 1))
-                sleep(sleep_duration)
-                nbr_of_retries += 1
+        if not self._connection_instance:
+            connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.server + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password
+            nbr_of_retries = 0
+            while True:
                 try:
-                    sqlstate = ex.args[1]
-                    sqlstate = sqlstate.split(".")
-                    logging.warning('Issue connecting to Lynx server, trying again')
-                    logging.warning(sqlstate[-3])
-                except Exception:
-                    logging.warning('Issue connecting to Lynx server and error response cannot be parsed. Trying again')
+                    self._connection_instance = pyodbc.connect(connection_string)
+                except pyodbc.Error as ex:
+                    if nbr_of_retries == int(self.CONN_RETRIES)-1:
+                        raise RuntimeError(f'Not able to establish connection with Lynx server after {self.CONN_RETRIES}') from ex
+                    sleep(int(self.BACKOFF_IN_SECONDS) * 2 ** nbr_of_retries + random.uniform(0, 1))
+                    nbr_of_retries += 1
+                    try:
+                        sqlstate = ex.args[1]
+                        sqlstate = sqlstate.split(".")
+                        logging.warning('Issue connecting to Lynx server, trying again')
+                        logging.warning(sqlstate[-3])
+                    except Exception:
+                        logging.warning('Issue connecting to Lynx server and error response cannot be parsed. Trying again')
+
     
     def query_to_list(self, query:str):
         """
@@ -115,12 +120,12 @@ class lynxClient():
         df = pd.DataFrame.from_records(rows, columns=columns)
         return df
 
-    def get_max_cursors(self) -> str:
+    def _get_max_cursors(self) -> str:
         cursor = self.conn.cursor()
         nbr_cursors = cursor.getinfo(pyodbc.SQL_MAX_CONCURRENT_ACTIVITIES)
         return nbr_cursors
 
-    def cursor(self):
+    def _get_cursor(self):
         cursor = self.conn.cursor()
         return cursor
 
@@ -140,9 +145,9 @@ class lynxClient():
                 
     def _stream_results(self, query:str, batch_size: int):
         cursor = self.conn.cursor()
-        iter = cursor.execute(query)
+        query_iterator = cursor.execute(query)
         while True:
-            rows = iter.fetchmany(batch_size)
+            rows = query_iterator.fetchmany(batch_size)
             if not rows:
                 break
             yield rows
@@ -163,5 +168,7 @@ class lynxClient():
 
 
 
+if __name__ == '__main__': 
 
+    lynx_instance = lynxClient()
         
