0b331f8c64ad9585c3f3f555ea55efd22ca9eaf1
pieterdepetter <pieter.de.petter@veb.be>
2023-04-07 15:40:50 +0200

    added download to s3, file match & list files function

--- src/pyveb/sharepoint_client.py
+++ src/pyveb/sharepoint_client.py
@@ -1,20 +1,15 @@
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.client_context import ClientContext
 from office365.sharepoint.files.file import File 
-import os
-import io
-from io import StringIO
-import json
-import pandas as pd
-import boto3
-import logging
+from office365.runtime.client_request_exception import ClientRequestException
+
+import os, io, logging, difflib, sys
 from datetime import datetime
 from dataclasses import dataclass
-from typing import List, Dict
-from office365.runtime.client_request_exception import ClientRequestException
-from office365.sharepoint.client_context import ClientContext
-import tempfile
+from typing import List
 
+from pyveb.s3_client import s3Client
+from pyveb.custom_decorators import retry
 
 @dataclass
 class sharepointFile:
@@ -24,22 +19,9 @@ class sharepointFile:
     url: str
     uri: str
     version: str
-    ServerRelativeUrl: str
-
-def parse_sharepoint_file_object(obj) -> sharepointFile : 
-    name = obj['Name']
-    last_modified_date = obj['TimeLastModified']
-    creation_date=obj['TimeCreated']
-    url = obj['LinkingUri']
-    uri = obj['LinkingUrl']
-    version = str(obj['MajorVersion'])+'.'+str(obj['MinorVersion'])
-    relative_url = obj['ServerRelativeUrl']
-    sp = sharepointFile(name, last_modified_date, creation_date, url, uri, version, relative_url)
-    return sp
+    relative_url: str
 
 class sharepointClient():
-
-    VEB_URL = 'https://vlaamsenergiebedrijf.sharepoint.com/leveringen/Marktwerking'
 
     def __init__(self, site_url ) -> None:
         """
@@ -61,9 +43,25 @@ class sharepointClient():
         except KeyError as e:
             logging.error('Couldnot find the required environment variables: SHAREPOINT_USER & SHAREPOINT_PASSWORD. Ensure they;re setup locally or injected into docker container via entrypoint.sh ')
         self.user_credentials = UserCredential(user, password)
-        self.ctx = ClientContext(site_url).with_credentials(self.user_credentials)
-        logging.info(f'Successfully established connection to sharepoint: {site_url}')
+        try:
+            self.ctx = ClientContext(site_url).with_credentials(self.user_credentials)
+            logging.info(f'Successfully established connection to sharepoint: {site_url}')
+        except ClientRequestException as e:
+            logging.error(f'Issue establishing connection to sharepoint: {site_url}. Exiting...')
+            sys.exit(1) 
         return
+
+    @staticmethod
+    def parse_sharepoint_file_object(obj) -> sharepointFile : 
+        name = obj['Name']
+        last_modified_date = obj['TimeLastModified']
+        creation_date=obj['TimeCreated']
+        url = obj['LinkingUri']
+        uri = obj['LinkingUrl']
+        version = str(obj['MajorVersion'])+'.'+str(obj['MinorVersion'])
+        relative_url = obj['ServerRelativeUrl']
+        sp = sharepointFile(name, last_modified_date, creation_date, url, uri, version, relative_url)
+        return sp
 
     def list_files(self, folder_prefix: str) -> List[sharepointFile]:
         """
@@ -90,42 +88,44 @@ class sharepointClient():
         self.ctx.load(libraryRoot).execute_query()
         files = libraryRoot.files
         self.ctx.load(files).execute_query()
-        parsed_files = [parse_sharepoint_file_object(f.properties) for f in files]
+        parsed_files = [self.parse_sharepoint_file_object(f.properties) for f in files]
         return parsed_files
     
-    def download_file(self, file):
-        File.download()
-        with tempfile.TemporaryDirectory() as local_path:
-            file_name = os.path.basename(file)
-            with open(os.path.join(local_path, file_name), 'wb') as local_file:
-                file = File.from_url(file).with_credentials(self.user_credentials).download(local_file).execute_query()
-            print("'{0}' file has been downloaded into {1}".format(file.serverRelativeUrl, local_file.name))
+    def match_filename(self, list_files: List[sharepointFile], file_name: str) -> sharepointFile:
+        files_found = [f.name for f in list_files]
+        logging.warning(f'All files in folder: {files_found}')
+        best_match = difflib.get_close_matches(file_name, files_found,1)
+        logging.warning(f'Closest match found: {best_match[0]} for orginal file name {file_name}')
+        best_file = [f for f in list_files if f.name == best_match[0]]
+        return best_file[0]
 
-    def download_rel_url(self, url):
-        download_path = os.path.join(tempfile.mkdtemp(), os.path.basename(url))
-        with open(download_path, "wb") as local_file:
-            _ = self.ctx.web.get_file_by_server_relative_url(url).download(local_file).execute_query()
-            #file = ctx.web.get_file_by_server_relative_url(file_url).download(local_file).execute_query()
-        print("[Ok] file has been downloaded into: {0}".format(download_path))
+    @retry(retries=3, error="Error download sharepoint file to s3")
+    def download_to_s3(self, sharepoint_folder_prefix, sharepoint_file_name, s3_prefix:str, s3_bucket: str ='veb-data-pipelines', **kwargs):
+        """
+            Downloads a sharepoint to the provided s3 prefix and bucket. 
 
-    def download(self, file):
-        current_file = File.open_binary(self.ctx, file.serverRelativeUrl)
+            ARGUMENTS:
+                -sharepoint_folder_prefix:  
+                    e.g.
+                        IF folder URL is https://vlaamsenergiebedrijf.sharepoint.com/leveringen/Marktwerking/Facturatie/Forms/Alle%20documenten.aspx 
+                        THEN folder_prefix = 'Facturatie'
 
-if __name__=='__main__':
+                        IF folder URL is https://vlaamsenergiebedrijf.sharepoint.com/leveringen/Marktwerking/Facturatie/Forms/Alle%20documenten.aspx?id=%2Fleveringen%2FMarktwerking%2FFacturatie%2FB%26O%20Facturatie&viewid=d82816a0%2D29b3%2D433d%2Db6de%2D9585c8984bd9
+                        THEN folder_prefix = 'Facturatie/B&O Facturatie
 
-    site_url = 'https://vlaamsenergiebedrijf.sharepoint.com/leveringen/Marktwerking'
-    folder_prefix = 'Facturatie/B&O Facturatie'
-    sp = sharepointClient(site_url)
-    files = sp.list_files(folder_prefix)
-    for f in files: print(f)
-    # logging.warning('downloading file')
-    # def remove_prefix(text, prefix):
-    #     return text[text.startswith(prefix) and len(prefix):]
-    
-    # url = remove_prefix(files[0].uri, f'{site_url}/')
-    # print(url.split('?')[0])
-    # sp.download_rel_url(url)
-    sp.download(f.rel_url)
-
-    # https://vlaamsenergiebedrijf.sharepoint.com/leveringen/Marktwerking/Facturatie/B&O%20Facturatie/Allocatie%20EL%202022%20V1.xlsb
- 
+                - s3_prefix: folder/subfolder
+                - s3_bucket: default veb-data-pipelines
+        """
+        files = self.list_files(sharepoint_folder_prefix)
+        file = self.match_filename(files, sharepoint_file_name)
+        current_file = File.open_binary(self.ctx, file.relative_url)
+        bytes_file_obj = io.BytesIO()
+        bytes_file_obj.write(current_file.content)
+        bytes_file_obj.seek(0)
+        s3 = s3Client(s3_bucket)
+        file_name = file.name.replace(' ', '_')
+        key = f'{s3_prefix}/{file_name}'
+        s3.client.put_object(Body=bytes_file_obj, Bucket=s3_bucket, Key=key)
+        logging.info(f'Wrote {file.name} to {s3_bucket}/{s3_prefix}/{file_name}')
+        return
+        
